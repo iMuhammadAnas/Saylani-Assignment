7dc8887aec7d566b9242a00248e617fa36fb1d48
Anas Khan <anasnaseem420@gmail.com>
2022-08-29 22:29:19 +0500

    Assignment

---
+++ task2.js
@@ -0,0 +1,14 @@
+let value ="madam" ;
+
+const revVlaue = () => {
+    let myRevVal = value.toString().split("").reverse().join("");
+    let myStrVal = value.toString();
+
+    if(myStrVal === myRevVal){
+        console.log("palindrome")
+    } else{
+        console.log("Not a palindrome")
+    }
+};
+
+revVlaue();
---
+++ task3.js
@@ -0,0 +1,6 @@
+var string = prompt("Please enter a string");
+var stringArray = string.split(" ");
+stringArray.sort();
+console.log(string);
+console.log(stringArray);
+console.log(stringArray.join(" "));
